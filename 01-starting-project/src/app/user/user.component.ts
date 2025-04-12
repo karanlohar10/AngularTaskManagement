d74186b4fe27bc266596ca8d5a343cb697224dba
Komal Lohar <karanlohar69@gmail.com>
2025-04-12 15:54:11 +0530

    feat(outputdecorator): getting output from a component using output decorator

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, signal, computed } from '@angular/core';
+import { Component, Input, input, Output, signal, computed, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,15 +11,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input() name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => `assets/users/${this.avatar()}`)
-  // get imagePath () {
-  //   return `assets/users/${this.avatar}`;
-  // }
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input() name!: string;
+
+  @Output() select = new EventEmitter();
+  
+  get imagePath () {
+    return `assets/users/${this.avatar}`;
+  }
+
   onSelectUser(){
-    
+    this.select.emit(this.id);
   }
 }
