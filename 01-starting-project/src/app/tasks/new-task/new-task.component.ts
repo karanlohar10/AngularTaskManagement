714ba06b6bce50af3c990ce51eaeaf52bd2d85b9
Komal Lohar <karanlohar69@gmail.com>
2025-04-15 20:02:37 +0530

    feat(angular): add new component for adding new task

---
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector:"app-new-task",
+    templateUrl:"./new-task.component.html",
+    styleUrl:"./new-task.component.css",
+    imports:[]
+})
+export class NewTaskComponent
+{
+    
+}
--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { TaskComponent } from "./task/task.component";
 import { dummyTasks } from "../dummy-tasks";
+import { Task } from "./task/task.model";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
     selector:'app-tasks',
     standalone: true,
-    imports: [TaskComponent],
+    imports: [TaskComponent, NewTaskComponent],
     styleUrl: './tasks.component.css',
     templateUrl: './tasks.component.html'
 })
@@ -13,9 +15,18 @@ export class TasksComponent
 {
     @Input({required: true}) userName !:string;
     @Input({required: true}) userId !: string;
+    isAddingTask : boolean = false;
 
+    myTasks:Task[] = dummyTasks; 
     get selectedUserTasks(){
-        return dummyTasks.filter((task) => task.userId === this.userId);
+        return this.myTasks.filter((task) => task.userId === this.userId);
     }
 
+    onCompleteTask(id: string){
+        this.myTasks = this.myTasks.filter((task) => task.id !== id);
+    }
+
+    onStartAddTask(){
+        this.isAddingTask = true;
+    }
 } 
