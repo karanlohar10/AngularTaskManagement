dcd5b2af1ecfa7ad0419cae56d5d11beb8edfa04
Komal Lohar <karanlohar69@gmail.com>
2025-04-15 21:21:21 +0530

    feat(directives): 2 way data binding using ngModel in case of both simple prop and signal

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     standalone: true,
     selector:"app-new-task",
     templateUrl:"./new-task.component.html",
     styleUrl:"./new-task.component.css",
-    imports:[]
+    imports:[FormsModule]
 })
 export class NewTaskComponent
 {
     @Output() cancel = new EventEmitter();
+    // enteredTitle : string = '';
+    enteredTitle = signal('');
 
     onCancel(){
         this.cancel.emit();
